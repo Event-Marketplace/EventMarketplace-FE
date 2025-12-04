47e4aeab7307e6495ebccb3bf9b0c6841292a516
Barteq14 <b.longota2@wp.pl>
2025-12-04 21:42:58 +0100

    Completed pagination & basic filters (will be improve)

--- src/app/(protected)/event/list/components/Filter.tsx
+++ src/app/(protected)/event/list/components/Filter.tsx
@@ -1,8 +1,82 @@
-const OgranizerEventListFilter = () => {
+import { useState } from "react";
+import Image from "next/image";
+import trashIcon from "@/images/trash.svg";
+
+type OrganizerEventFilterProps = {
+  onChange: (filters: {
+    title?: string;
+    status?: string;
+    startDate?: string;
+    endDate?: string;
+  }) => void;
+  onClear: () => void;
+};
+
+const OgranizerEventListFilter = ({
+  onChange,
+  onClear,
+}: OrganizerEventFilterProps) => {
+  const [title, setTitle] = useState("");
+  const [status, setStatus] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const handleApply = () => {
+    onChange({ title, status, startDate, endDate });
+  };
+
+  const handleClear = () => {
+    setTitle("");
+    setStatus("");
+    setStartDate("");
+    setEndDate("");
+    onClear();
+  };
+
   return (
-    <div className="">
-      <div className="">Filrtowanie tutaj</div>
-    </div>
+    <>
+      {/* Input tekstowy */}
+      <input
+        type="text"
+        placeholder="Szukaj wydarzenia..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="border p-2 rounded flex-1"
+      />
+
+      <input
+        type="text"
+        placeholder="Szukaj wydarzenia..."
+        value={status}
+        onChange={(e) => setStatus(e.target.value)}
+        className="border p-2 rounded flex-1"
+      />
+
+      <div className="flex gap-2 md:flex flex-wrap">
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+          className="sm:w-auto w-full border p-2 rounded"
+        />
+        <input
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+          className="sm:w-auto w-full border p-2 rounded"
+        />
+      </div>
+
+      <button onClick={handleClear} className="cursor-pointer">
+        <Image src={trashIcon} alt="sd" height={32} />
+      </button>
+      <button
+        onClick={handleApply}
+        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer"
+      >
+        Filtruj
+      </button>
+    </>
   );
 };
 
--- src/app/(protected)/event/list/components/OrganizerEventList.tsx
+++ src/app/(protected)/event/list/components/OrganizerEventList.tsx
@@ -6,15 +6,29 @@ import { useEffect, useState } from "react";
 import { Event } from "@/lib/interfaces";
 import { apiAxios } from "@/lib/apiAxios";
 import OrganizerEventCard from "./Card";
+import ListWrapper from "@/components/ui/list/ListWrapper";
+
+type FilterProps = {
+  title?: string;
+  status?: string;
+  startDate?: string;
+  endDate?: string;
+};
 
 const OrganizerEventList = () => {
-  const [events, setEvents] = useState<Event[] | undefined>([]);
+  const [events, setEvents] = useState<Event[]>([]);
   const [totalItems, setTotalItems] = useState<number>(0);
+  const [page, setPage] = useState<number>(1);
+  const [filters, setFilters] = useState<FilterProps>();
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const res = await apiAxios.get("Event/organizer");
+        const res = await apiAxios.get("Event/organizer", {
+          params: {
+            pageNumber: page,
+          },
+        });
         setEvents(res.data.events);
         setTotalItems(res.data.totalCount);
         console.log("res", res);
@@ -24,18 +38,65 @@ const OrganizerEventList = () => {
     };
 
     fetchEvents();
-  }, []);
+  }, [page]);
+
+  const handleChangePage = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleFilter = async (newFilters: FilterProps) => {
+    setFilters(newFilters);
+    try {
+      const res = await apiAxios.get("Event/organizer", {
+        params: {
+          pageNumber: page,
+          title: newFilters.title,
+          status: newFilters.status,
+          startDate: newFilters.startDate,
+          endDate: newFilters.endDate,
+        },
+      });
+      setEvents(res.data.events);
+      setTotalItems(res.data.totalCount);
+    } catch (err) {
+      console.log("err");
+    }
+  };
 
   return (
-    <div className="w-full flex flex-col gap-10">
+    <div className="flex flex-col gap-4">
       <OrganizerEventListHeader />
-      <OgranizerEventListFilter />
-
-      <div className="flex flex-col gap-2">
-        {events?.map((event) => (
-          <OrganizerEventCard key={event.id} event={event} />
-        ))}
-      </div>
+      <ListWrapper
+        data={{
+          items: events,
+          totalCount: totalItems,
+          totalPages: Math.ceil(totalItems / 9),
+          currentPage: page,
+        }}
+        onPageChange={handleChangePage}
+        filters={
+          <OgranizerEventListFilter
+            onChange={handleFilter}
+            onClear={() =>
+              handleFilter({
+                title: "",
+                status: undefined,
+                startDate: "",
+                endDate: "",
+              })
+            }
+          />
+        }
+        visibleTotalCount={true}
+      >
+        <div className="w-full flex flex-col gap-10">
+          <div className="flex flex-col gap-2">
+            {events?.map((event) => (
+              <OrganizerEventCard key={event.id} event={event} />
+            ))}
+          </div>
+        </div>
+      </ListWrapper>
     </div>
   );
 };
