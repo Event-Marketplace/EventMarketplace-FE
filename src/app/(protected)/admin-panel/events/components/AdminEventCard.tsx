574f0be1b21f51112af5e18f2243b71327500b43
Barteq14 <b.longota2@wp.pl>
2026-01-19 16:44:43 +0100

    Modified code -  rejection event

--- src/app/(protected)/admin-panel/events/components/AdminEventCard.tsx
+++ src/app/(protected)/admin-panel/events/components/AdminEventCard.tsx
@@ -19,6 +19,10 @@ import { AppState } from "@/redux/store";
 import { useEventComments } from "../hooks/useEventComments";
 import { errors } from "jose";
 import { apiAxios } from "@/lib/apiAxios";
+import InfoModalEM from "@/components/ui/modals/InfoModalEM";
+import { Field, Form, Formik } from "formik";
+import InputEM from "@/components/ui/InputEM";
+import toast from "react-hot-toast";
 
 type AdminEventCardProps = {
   event: AdminEvent;
@@ -26,6 +30,10 @@ type AdminEventCardProps = {
   handleImageModal: (url: string) => void;
   handleOpenDescModal: (content: string) => void;
   onSuccess?: () => void;
+};
+
+type RejectionReasonProps = {
+  rejectionReason: string;
 };
 
 const AdminEventCard = ({
@@ -36,7 +44,7 @@ const AdminEventCard = ({
   onSuccess,
 }: AdminEventCardProps) => {
   const [openCommentModal, setOpenCommentModal] = useState<boolean>(false);
-
+  const [openInfoModal, setOpenInfoModal] = useState<boolean>(false);
   const userId = useSelector((state: AppState) => state.auth.userId);
   const currentContext = localStorage.getItem("currentContext");
 
@@ -70,6 +78,41 @@ const AdminEventCard = ({
     }
   };
 
+  const initialValues: RejectionReasonProps = {
+    rejectionReason: "",
+  };
+
+  const handleRejectEvent = async (
+    eventId: string,
+    values: RejectionReasonProps
+  ) => {
+    if (!values.rejectionReason) {
+      toast.error(`Musisz wprowadzić powód odrzucenia!`);
+      return;
+    }
+    console.log("eventid", eventId);
+    console.log("reason", values.rejectionReason);
+    try {
+      await apiAxios.put(`Event/reject-event/${eventId}`, {
+        rejectionReason: values.rejectionReason,
+      });
+      setOpenInfoModal(false);
+      setTimeout(() => {
+        if (onSuccess) {
+          onSuccess();
+        }
+      }, 1500);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleCloseInfoModal = () => {
+    setOpenInfoModal(false);
+  };
+
+  const formId = `RejectionEventModal-${event.id}`;
+  console.log("formid", formId);
   return (
     <>
       <div className="w-1/12 flex flex-col">
@@ -148,6 +191,9 @@ const AdminEventCard = ({
             src={circleCheck}
             width={28}
             alt="approve-icon"
+            onClick={() => {
+              handleApproveEvent(event.id);
+            }}
           />
           <Image
             className="hover:cursor-pointer"
@@ -155,7 +201,7 @@ const AdminEventCard = ({
             width={28}
             alt="reject-icon"
             onClick={() => {
-              handleApproveEvent(event.id);
+              setOpenInfoModal(true);
             }}
           />
 
@@ -185,6 +231,36 @@ const AdminEventCard = ({
             onSuccess={onSuccess}
             formId="admin-comment-form"
           />
+
+          <InfoModalEM
+            onCancel={handleCloseInfoModal}
+            setOpen={openInfoModal}
+            actionBtn
+            formId={formId}
+          >
+            <div className="flex flex-col gap-3">
+              <h1 className="text-2xl">
+                Czy na pewno chcesz odrzucić to wydarzenie?
+              </h1>
+              <Formik
+                onSubmit={(values) => {
+                  handleRejectEvent(event.id, values);
+                }}
+                initialValues={initialValues}
+              >
+                <Form id={formId}>
+                  <Field
+                    as={InputEM}
+                    type="text"
+                    name="rejectionReason"
+                    textarea
+                    height={100}
+                    placeholder="Wpisz powód"
+                  />
+                </Form>
+              </Formik>
+            </div>
+          </InfoModalEM>
         </div>
       )}
     </>
--- src/components/ui/modals/InfoModalEM.tsx
+++ src/components/ui/modals/InfoModalEM.tsx
@@ -5,9 +5,17 @@ type ModalProps = {
   children: React.ReactNode;
   onCancel: () => void;
   setOpen: boolean;
+  actionBtn?: boolean;
+  formId?: string;
 };
 
-const InfoModalEM = ({ children, onCancel, setOpen }: ModalProps) => {
+const InfoModalEM = ({
+  children,
+  onCancel,
+  setOpen,
+  actionBtn,
+  formId,
+}: ModalProps) => {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
@@ -27,13 +35,26 @@ const InfoModalEM = ({ children, onCancel, setOpen }: ModalProps) => {
         ></div>
         <div className="relative bg-white rounded-2xl shadow-lg w-full max-w-lg mx-4 p-6 animate-[fadeIn_0.2s_ease-out]">
           <div className="text-gray-700">{children}</div>
-          <div className="flex justify-end gap-2 mt-6">
+          <div
+            className={`flex ${
+              actionBtn ? "justify-between" : "justify-end"
+            }  gap-2 mt-6`}
+          >
             <Button
               onClick={onCancel}
               className="px-4 py-2 rounded-lg hover:cursor-pointer border border-black-700 bg-gray-100 hover:bg-gray-200 transition text-black"
             >
               Zamknij
             </Button>
+            {actionBtn && (
+              <Button
+                type="submit"
+                form={formId}
+                className="px-4 py-2 rounded-lg hover:cursor-pointer border border-black-700 bg-blue-300 hover:bg-blue-400 transition text-black"
+              >
+                Potwierdź
+              </Button>
+            )}
           </div>
         </div>
       </div>
